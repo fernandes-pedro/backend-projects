a7e6757524c00fee4817e42fa7c418758363611c
Pedro Fernandes <edpedro12345@gmail.com>
2025-07-04 00:36:01 -0300

    feat: create basic folder/file

---
+++ fundamentos/gerenciador.py
@@ -0,0 +1,11 @@
+def mostrarMenu():
+    print("----- Menu de interacao -----")
+    print("---> 1. Adicionar Tarefa <--")
+    print("---> 2. Atualizar Tarefa <--")
+    print("---> 3. Excluir Tarefa   <--")
+    print("---> 4. Concluir         <--")
+    print("---> 5. Sair             <--")
+    print("Escolha uma opcao de 1 a 5:")
+
+while True:
+    mostrarMenu()
