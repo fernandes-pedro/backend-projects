37c7751bd2858e3f0e4d14ab998d33394aad7c00
Pedro Fernandes <edpedro12345@gmail.com>
2025-07-06 23:56:56 -0300

    feat: update functions

--- fundamentos/gerenciador.py
+++ fundamentos/gerenciador.py
@@ -1,16 +1,22 @@
 def mostrarMenu():
     print("----- Menu de interacao -----")
     print("---> 1. Adicionar Tarefa <--")
-    print("---> 2. Atualizar Tarefa <--")
-    print("---> 3. Excluir Tarefa   <--")
-    print("---> 4. Concluir         <--")
-    print("---> 5. Sair             <--")
+    print("---> 2. Exibir Tarefas   <--")
+    print("---> 3. Atualizar Tarefa <--")
+    print("---> 4. Excluir Tarefa   <--")
+    print("---> 5. Concluir         <--")
+    print("---> 6. Sair             <--")
 
 lista_tarefas = []
 
 def adicionarTarefa(tarefa):
     lista_tarefas.append(tarefa)
     print("Tarefa adicionada com sucesso!")
+
+def exibirTarefas():
+    print("As tarefas cadastradas no sistema sao: ")
+    for tarefa in lista_tarefas:
+        print(tarefa)
 
 def atualizarTarefa(buscada, tarefa_nova):
     if buscada in lista_tarefas:
@@ -30,24 +36,28 @@ def concluirTarefa():
 
 while True:
     mostrarMenu()
-    opcao = input("Escolha uma opcao de 1 a 5:")
-    if opcao == "1":
+    opcao = int(input("Escolha uma opcao de 1 a 6: "))
+
+    if opcao == 1:
         tarefa = input("Digite qual tarefa a ser adicionada: ")
         adicionarTarefa(tarefa)
 
-    elif opcao == "2":
+    elif opcao == 2:
+        exibirTarefas()
+
+    elif opcao == 3:
         buscada = input("Digite qual tarefa a ser atualizada: ")
         tarefa_nova = ("Digita o novo nome da tarefa: ")
         atualizarTarefa(buscada, tarefa_nova)
 
-    elif opcao == "3": 
+    elif opcao == 4: 
         tarefa = input("Digite qual tarefa a ser excluida: ")
         excluirTarefa()
 
-    elif opcao == "4":
+    elif opcao == 5:
         concluirTarefa()
         
-    elif opcao == "5":
+    elif opcao == 6:
         print("Saindo...")
         break
     else:
