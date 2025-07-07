3ba6c6a2847e08f3286df89a754c7ed947d2ee97
Pedro Fernandes <edpedro12345@gmail.com>
2025-07-07 02:48:10 -0300

    feat: final update

--- fundamentos/gerenciador.py
+++ fundamentos/gerenciador.py
@@ -7,40 +7,53 @@ def mostrarMenu():
     print("---> 5. Concluir         <--")
     print("---> 6. Sair             <--")
 
-lista_tarefas = []
+lista_tarefas = {"Refatorar": False, "Dormir": False, "Commitar": True}
 
 def adicionarTarefa(tarefa):
-    lista_tarefas.append(tarefa)
-    print("Tarefa adicionada com sucesso!")
+    if tarefa in lista_tarefas: 
+        print("A tarefa ja existe!")
+    else:
+        lista_tarefas[tarefa] = False
+        print("Tarefa adicionada com sucesso!")
 
 def exibirTarefas():
     print("As tarefas cadastradas no sistema sao: ")
-    for tarefa in lista_tarefas:
-        print(tarefa)
+    for tarefa, concluida in lista_tarefas.items(): 
+        status = "concluida" if concluida else "pendente"
+        print(f"{tarefa} - [{status}]")
 
 def atualizarTarefa(buscada, tarefa_nova):
     if buscada in lista_tarefas:
-        index = lista_tarefas.index(buscada)
-        lista_tarefas[index] = tarefa_nova
+        lista_tarefas[tarefa_nova] = lista_tarefas.pop(buscada)
         print("Tarefa atualizada com sucesso!")
     else:
         print('tarefa nao encontrada')
 
-def excluirTarefa():
-    lista_tarefas.pop()
-    print("Tarefa excluida com sucesso!")
+def excluirTarefa(tarefa_excluida):
+    if tarefa_excluida in lista_tarefas: 
+        del lista_tarefas[tarefa_excluida]
+        print("Tarefa excluida com sucesso!")
+    else:
+        print("Tarefa nao encontrada!")
 
-def concluirTarefa():
-    print(lista_tarefas)
-    print("Tarefa concluida com sucesso!")
+def concluirTarefa(tarefa_buscada):
+    if tarefa_buscada in lista_tarefas:
+        lista_tarefas[tarefa_buscada] = True
+        print("Tarefa concluida com sucesso!")
+    else:
+        print("Tarefa nao encontrada!")
 
 while True:
     mostrarMenu()
-    opcao = int(input("Escolha uma opcao de 1 a 6: "))
+    try:
+        opcao = int(input("Escolha uma opção de 1 a 6: "))
+    except ValueError:
+        print("Por favor, digite um número válido!")
+        continue
 
     if opcao == 1:
-        tarefa = input("Digite qual tarefa a ser adicionada: ")
-        adicionarTarefa(tarefa)
+        tarefa_adicionada = input("Digite qual tarefa a ser adicionada: ")
+        adicionarTarefa(tarefa_adicionada)
 
     elif opcao == 2:
         exibirTarefas()
